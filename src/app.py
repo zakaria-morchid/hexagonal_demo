dd4c06e309b9a6edc16c80abaa4eb309de84219c
zakaria.morchid-ext <zakaria.morchid-ext@mousquetaires.com>
2025-07-05 17:15:27 +0200

    solution 2

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from mocks.gitlab import gl
 from mocks.github import gh
 import argparse
-from usecases.list_merge_requests import display_merge_requests
+from usecases.list_merge_requests import ListMergeRequests
 from providers.gitlab_provider import GitLabProvider
 from providers.github_provider import GitHubProvider
 
@@ -15,5 +15,4 @@ if __name__ == "__main__":
     args = parse_args()
 
     providers = [GitLabProvider(gl), GitHubProvider(gh)]
-    for provider in providers:
-        display_merge_requests(provider, username=args.username)
+    ListMergeRequests(providers).execute(username=args.username)
--- src/usecases/list_merge_requests.py
+++ src/usecases/list_merge_requests.py
@@ -1,8 +1,28 @@
-def display_merge_requests(provider, username=None):
-    print(f"🛠️  [{provider.name}]")
-    for mr in provider.list_merge_requests(username=username):
-        print(f"  ▪ {mr['project']}")
-        print(f"  ▪ {mr['title']} ({mr['state']})")
-        print(f"    Auteur : {mr['author']}")
-        for approval in mr["approvals"]:
-            print(f"     - {'OK' if approval['approved'] else '  '} {approval['name']}")
+from abc import ABC, abstractmethod
+
+class IListMergeRequestsUseCase(ABC):
+    @abstractmethod
+    def execute(self, username=None):
+        """
+        Exécute le cas d’usage de listing des merge requests,
+        avec un filtrage optionnel par username.
+        """
+        pass
+
+
+class ListMergeRequests(IListMergeRequestsUseCase):
+    def __init__(self, providers):
+        self.providers = providers
+
+    def execute(self, username=None):
+        for provider in self.providers:
+            self._display_merge_requests(provider, username=username)
+        
+    def _display_merge_requests(self, provider, username=None):
+        print(f"🛠️  [{provider.name}]")
+        for mr in provider.list_merge_requests(username=username):
+            print(f"  ▪ {mr['project']}")
+            print(f"  ▪ {mr['title']} ({mr['state']})")
+            print(f"    Auteur : {mr['author']}")
+            for approval in mr["approvals"]:
+                print(f"     - {'OK' if approval['approved'] else '  '} {approval['name']}")
